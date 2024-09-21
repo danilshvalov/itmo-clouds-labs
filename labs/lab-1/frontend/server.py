38fa689424184feccd2af1c01cbf70a143cb21ca
Daniil Shvalov <57654917+danilshvalov@users.noreply.github.com>
2024-09-21 15:12:50 +0300

    feat(labs): lab 1 (#1)

---
+++ labs/lab-1/frontend/server.py
@@ -0,0 +1,49 @@
+import argparse
+import random
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+    def _message(self):
+        with open("index.html", "rb") as file:
+            return file.read()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._message())
+
+    def do_HEAD(self):
+        self._set_headers()
+
+
+def run(server_class=HTTPServer, handler_class=Server, addr='localhost', port=3000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f'Starting httpd server on {addr}:{port}')
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Run a simple HTTP server')
+    parser.add_argument(
+        '-l',
+        '--listen',
+        default='localhost',
+        help='Specify the IP address on which the server listens',
+    )
+    parser.add_argument(
+        '-p',
+        '--port',
+        type=int,
+        default=3000,
+        help='Specify the port on which the server listens',
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
