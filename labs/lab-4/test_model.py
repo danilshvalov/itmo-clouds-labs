e9cd34cba7aaef4fd8e4f238fe69d5ec8773d24b
RomanKharkovskoy <roman.kharkovskoy@yandex.ru>
2024-10-13 14:03:33 +0300

    test changes for ci/cd

--- labs/lab-4/test_model.py
+++ labs/lab-4/test_model.py
@@ -1,4 +1,5 @@
 from model import create_model
+import pandas as pd
 import unittest
 
 
@@ -11,6 +12,20 @@ class TestModel(unittest.TestCase):
         model = create_model()
         self.assertEqual(model.__class__.__name__, "LogisticRegression",
                          "Model should be LogisticRegression")
+
+    def test_model_training(self):
+        model = create_model()
+        data = pd.DataFrame({
+            'feature1': [1.0, 2.0, 3.0, 4.0, 5.0],
+            'feature2': [2.0, 3.0, 3.5, 5.0, 5.5],
+            'target': [0, 0, 1, 1, 1]
+        })
+        X = data[['feature1', 'feature2']]
+        y = data['target']
+        model.fit(X, y)
+        predictions = model.predict(X)
+        self.assertEqual(len(predictions), len(
+            y), "Predictions should match the number of samples")
 
 
 if __name__ == '__main__':
